6a01b8ff2f3315ffb0d493968d08443626ccef5c
AlejandroZitzuMoto <ja.zitzumbomontano>
2024-04-05 20:45:02 -0600

    Raiz

--- Raiz/Raices/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from .heramientas.comandos import es_flotante, intr_int, sigma, localizador, suma, derivada, Dominio, error, derivada
-
-from .biseccion import biseccion, raiz_b
-from .Newton import Newton, raiz_N 
-from .punto_fijo import punto_fijo, raiz_fija
-from .secante1 import secante1, raiz_secante1
-from .secante2 import secante2, raiz_secante2
-
-__all__ = ['es_flotante', 'intr_int', 'sigma', 'localizador', 'suma', 'derivada', 'Dominio', 'biseccion', 'raiz_b', 'Newton', 'raiz_N', 'punto_fijo', 'raiz_fija', 'secante1', 'raiz_secante1', 'secante2', 'raiz_secante2', 'error', 'derivada']
